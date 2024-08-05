9e88f87f7314978d487af11cb25d47899823ea78
Daria Dmitrienko <ishmizh@gmail.com>
2024-08-05 19:34:24 +0400

    render  ProductView

--- src/components/ProductView.ts
+++ src/components/ProductView.ts
@@ -1,15 +1,24 @@
 import { IProductData } from "../types/contracts";
+import { ensureElement } from "../utils/utils";
 import { View } from "./base/View";
 
 export class ProductView extends View<IProductData> {
+
+  private readonly title: HTMLHeadingElement;
+  private readonly image: HTMLImageElement;
+  private readonly price: HTMLElement;
+
   constructor(element: HTMLElement) {
     super(element);
-
-    // ...
+    this.title = ensureElement<HTMLHeadingElement>(".card__title", this.element);
+    this.image = ensureElement<HTMLImageElement>(".card__image", this.element);
+    this.price = ensureElement<HTMLElement>(".card__price", this.element);
   }
 
   render(data: IProductData): HTMLElement {
-    this.element.style.backgroundColor = '#0000ff';
+    this.title.textContent = data.title;
+    this.image.src = `${data.image}`;
+    this.price.textContent = `${data.price}`;
     return this.element;
   }
 }
