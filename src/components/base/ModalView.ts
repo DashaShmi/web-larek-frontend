c86d1a9225c9f3b74bfc50581c5ee37922ef8548
Daria Dmitrienko <ishmizh@gmail.com>
2024-08-09 17:06:38 +0400

    modal закрытие на крестик

--- src/components/base/ModalView.ts
+++ src/components/base/ModalView.ts
@@ -1,26 +1,24 @@
 import { IModalData } from "../../types/contracts";
+import { ensureElement } from "../../utils/utils";
 import { View } from "./View";
 
-// Класс ModalView
-// Реализует модальное окно. Так же предоставляет методы `open`, `close` и `render` для управления отображением модального окна. Устанавливает слушатели на клавиатуру, для закрытия модального окна по Esc, на клик в оверлей и кнопку-крестик для закрытия попапа. 
-// interface IModalData{
-//   content: HTMLElement|View;
-// }
-// ```
-// class ModalView extends View<IModalData> {
-//  open(): void;
-//  close(): void;
-//  render(data: Partial<IModalData>){
-//     ... // отображение дочернего View}
-// }
-
 export class ModalView extends View<IModalData> {
+
+  constructor(element: HTMLElement) {
+    super(element);
+    const closeButtonElement = ensureElement('.modal__close', element);
+    closeButtonElement.addEventListener("click", this.close.bind(this));
+  }
+
+
   open(): void {
     this.element.classList.add('modal_active');
+    document.addEventListener("keyup", this.handleEscUp);
   };
 
   close(): void {
     this.element.classList.remove('modal_active');
+    document.removeEventListener("keyup", this.handleEscUp);
   };
 
   render(data: Partial<IModalData>) {
@@ -29,4 +27,12 @@ export class ModalView extends View<IModalData> {
 
     return this.element;
   }
+
+  handleEscUp = (evt: KeyboardEvent) => {
+    if (evt.key === "Escape") {
+      this.close();
+
+      console.log('escape');
+    }
+  };
 }
