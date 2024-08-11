174b5facafb5c44a613c7dd7a02a4533811e1a7d
Daria Dmitrienko <ishmizh@gmail.com>
2024-08-11 21:31:44 +0400

    заполнение ul

--- src/components/BasketView.ts
+++ src/components/BasketView.ts
@@ -1,8 +1,28 @@
 import { IBasketData } from "../types/contracts";
+import { cloneTemplate, ensureElement } from "../utils/utils";
 import { View } from "./base/View";
 
 export class BasketView extends View<IBasketData> {
+  private readonly listUl: HTMLElement;
+
+  constructor(element: HTMLElement) {
+    super(element);
+    this.listUl = ensureElement<HTMLElement>(".basket__list", this.element);
+  }
+
   override render(data: IBasketData): HTMLElement {
+
+    for (let i = 0; i < data.products.length; i++) {
+      const productData = data.products[i];
+
+      const li = cloneTemplate('#card-basket');
+      // заполнение данными
+
+      this.listUl.appendChild(li);
+
+    }
+
+
 
     return this.element;
   }
