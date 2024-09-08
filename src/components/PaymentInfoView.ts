1130a2356e8a4e05fc94cb4c164637e33806f950
Daria Dmitrienko <ishmizh@gmail.com>
2024-09-08 19:30:38 +0400

    пофиксила пеймент методы

--- src/components/PaymentInfoView.ts
+++ src/components/PaymentInfoView.ts
@@ -17,8 +17,21 @@ export class PaymentInfoView extends ViewWithForm<IFormDataWithErrors<IPaymentIn
     this.buttonOffline = ensureElement<HTMLButtonElement>('button[name="cash"]', this.element);
     this.inputAdress = ensureElement<HTMLInputElement>('input[name="address"]', this.element);
     this.formErrors = ensureElement<HTMLElement>(".form__errors", this.element);
-  }
 
+    this.buttonOnline.addEventListener('click', (event) => {
+      this.events.emit('paymentsInfo:input-change', {
+        name: 'paymentMethod',
+        value: "online"
+      })
+    })
+
+    this.buttonOffline.addEventListener('click', (event) => {
+      this.events.emit('paymentsInfo:input-change', {
+        name: 'paymentMethod',
+        value: "offline"
+      })
+    })
+  }
 
   protected override onInputChange(name: string, value: string): void {
     this.events.emit('paymentsInfo:input-change', {
@@ -35,6 +48,16 @@ export class PaymentInfoView extends ViewWithForm<IFormDataWithErrors<IPaymentIn
     this.formErrors.textContent = Object.values(data.errors).filter(x => x.trim().length > 0).join('; ');
 
     this.submitButton.disabled = !data.isValid;
+
+    this.buttonOffline.classList.remove('button_alt-active');
+    this.buttonOnline.classList.remove('button_alt-active');
+
+    if (data.value.paymentMethod === "offline") {
+      this.buttonOffline.classList.add('button_alt-active');
+    }
+    else if (data.value.paymentMethod === "online") {
+      this.buttonOnline.classList.add('button_alt-active');
+    }
 
     return this.element;
 
