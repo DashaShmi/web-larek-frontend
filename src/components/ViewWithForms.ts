ee5da4deae6f108949858ffec83456e66c75af45
Daria Dmitrienko <ishmizh@gmail.com>
2024-09-09 20:05:38 +0400

    почистила вью формы

--- src/components/ViewWithForms.ts
+++ src/components/ViewWithForms.ts
@@ -4,14 +4,10 @@ import { IEvents } from "./base/events";
 
 
 export abstract class ViewWithForm<T> extends ViewWithEvents<T> {
-
   protected form: HTMLFormElement;
-  protected formName: string;
-  protected inputs: NodeListOf<HTMLInputElement>;
-  // protected errors: Record<string, HTMLElement>;
   protected submitButton: HTMLButtonElement;
 
-  protected abstract onSubmit(formData: Record<string, string>): void;
+  protected abstract onSubmit(): void;
   protected abstract onInputChange(name: string, value: string): void;
 
   constructor(element: HTMLElement, events: IEvents) {
@@ -25,9 +21,6 @@ export abstract class ViewWithForm<T> extends ViewWithEvents<T> {
 
     this.submitButton = ensureElement<HTMLButtonElement>('.button', this.form);
 
-    this.inputs = this.form.querySelectorAll<HTMLInputElement>('.form__input');
-    this.formName = this.form.getAttribute('name') ?? 'noname';
-
     // слушаем инпуты
     this.form.addEventListener('input', (e) => {
       const target = e.target as HTMLInputElement;
@@ -38,18 +31,7 @@ export abstract class ViewWithForm<T> extends ViewWithEvents<T> {
     // слушаем сабмит
     this.form.addEventListener('submit', (evt) => {
       evt.preventDefault();
-      const formData = this.getInputValues();
-      this.onSubmit(formData);
+      this.onSubmit();
     });
   }
-
-  protected getInputValues() {
-    const valuesObject: Record<string, string> = {};
-    this.inputs.forEach((element) => {
-      valuesObject[element.name] = element.value;
-    });
-    return valuesObject;
-  }
-
-
 }
